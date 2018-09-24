7c0ebc6dc9acf729c6f2bbeeafe5ae10edcede10
Andrew <Andrew>
2018-09-24 12:48:18 +0300

    task1 reload

--- task1.py
+++ task1.py
@@ -1,18 +1,24 @@
-from random import random
-n = round(random() * 100)
-i = 1
-print("You have ten attempts to guess the result!")
-while i <= 10:
-    u = int(input(str(i) + ' attempt: '))
-    if u > n:
-        print('So much')
-    elif u < n:
-        print('So little')
+def task1():
+    from random import random
+    n = round(random() * 100)
+    i = 1
+    print("You have ten attempts to guess the result!")
+    while i <= 10:
+        try:
+            u = int(input(str(i) + ' attempt: '))
+        except (ValueError, UnboundLocalError):
+            print("Enter an integer, try again")
+        else:
+            if u > n:
+                print('So much')
+            elif u < n:
+                print('So little')
+            else:
+                print('You\'ve guessed from %d attempt' % i)
+                break
+        i += 1
     else:
-        print('You\'ve guessed from %d attempt' % i)
-        break
-    i += 1
-else:
-    print('You have  axhausted the 10 attempts. The number was', n)
+        print('You have  axhausted the 10 attempts. The number was', n)
 
 
+print(task1())
